3c3b87bfa691650d270059f97f460ad1410b1212
arthursmarinho <arthursnat@gmail.com>
2026-04-09 12:11:24 -0300

    improve: skeletons aparecem antes do conteudo a ser carregado

--- app/(main)/dashboard/components/HistoryTable.tsx
+++ app/(main)/dashboard/components/HistoryTable.tsx
@@ -1,5 +1,7 @@
 "use client";
+import { Skeleton } from "@/components/ui/skeleton";
 import { Trash2, CalendarDays, MessageSquare } from "lucide-react";
+import { useEffect, useState } from "react";
 
 export function HistoryTable({
   entries,
@@ -22,7 +24,11 @@ export function HistoryTable({
         return "😶";
     }
   };
-
+  const [isloading, setIsLoading] = useState(true);
+  useEffect(() => {
+    const timer = setTimeout(() => setIsLoading(false), 2000);
+    return () => clearTimeout(timer);
+  }, []);
   return (
     <div className="bg-white p-8 rounded-[2.5rem] shadow-sm border border-gray-100 h-full">
       <div className="flex justify-between items-center mb-8">
@@ -40,58 +46,69 @@ export function HistoryTable({
             </tr>
           </thead>
           <tbody>
-            {entries?.map((item) => (
-              <tr
-                key={item.id}
-                className="group hover:bg-gray-50 transition-colors"
-              >
-                <td className="bg-gray-50/50 rounded-l-2xl px-4 py-4 text-xs font-bold text-gray-500">
-                  {new Date(item.publishedAt).toLocaleDateString("pt-BR")}
-                </td>
-
-                <td className="px-4 py-4 text-2xl text-center">
-                  {/* AJUSTE AQUI: Se no Prisma o Int é 'moods', use item.moods */}
-                  {getEmoji(item.moods ?? item.mood)}
-                </td>
-
-                <td className="px-4 py-4">
-                  <div className="flex flex-col gap-2">
-                    <div className="flex flex-wrap gap-1">
-                      {Array.isArray(item.habits) ? (
-                        item.habits.map((habit: string, i: number) => (
-                          <span
-                            key={i}
-                            className="bg-purple-50 text-[#6C5CE7] text-[9px] px-2 py-1 rounded-lg font-bold"
-                          >
-                            {habit}
-                          </span>
-                        ))
-                      ) : (
-                        <span className="text-[9px] text-gray-400 italic">
-                          Sem hábitos
-                        </span>
-                      )}
-                    </div>
-
-                    {item.content && (
-                      <div className="flex items-start gap-1 text-gray-400 italic text-[11px]">
-                        <MessageSquare size={10} className="mt-0.5" />
-                        <p>{item.content}</p>
-                      </div>
-                    )}
-                  </div>
-                </td>
-
-                <td className="bg-gray-50/50 rounded-r-2xl px-4 py-4 text-right">
-                  <button
-                    onClick={() => onDelete(item.id)}
-                    className="text-gray-300 hover:text-red-400 transition-colors"
-                  >
-                    <Trash2 size={18} />
-                  </button>
+            {isloading ? (
+              <tr>
+                <td colSpan={4}>
+                  <Skeleton className="h-10 w-full" />
+                  <Skeleton className="h-10 w-full" />
+                  <Skeleton className="h-10 w-full" />
+                  <Skeleton className="h-10 w-full" />
+                  <Skeleton className="h-10 w-full" />
                 </td>
               </tr>
-            ))}
+            ) : (
+              entries?.map((item) => (
+                <tr
+                  key={item.id}
+                  className="group hover:bg-gray-50 transition-colors"
+                >
+                  <td className="bg-gray-50/50 rounded-l-2xl px-4 py-4 text-xs font-bold text-gray-500">
+                    {new Date(item.publishedAt).toLocaleDateString("pt-BR")}
+                  </td>
+
+                  <td className="px-4 py-4 text-2xl text-center">
+                    {getEmoji(item.moods ?? item.mood)}
+                  </td>
+
+                  <td className="px-4 py-4">
+                    <div className="flex flex-col gap-2">
+                      <div className="flex flex-wrap gap-1">
+                        {Array.isArray(item.habits) ? (
+                          item.habits.map((habit: string, i: number) => (
+                            <span
+                              key={i}
+                              className="bg-purple-50 text-[#6C5CE7] text-[9px] px-2 py-1 rounded-lg font-bold"
+                            >
+                              {habit}
+                            </span>
+                          ))
+                        ) : (
+                          <span className="text-[9px] text-gray-400 italic">
+                            Sem hábitos
+                          </span>
+                        )}
+                      </div>
+
+                      {item.content && (
+                        <div className="flex items-start gap-1 text-gray-400 italic text-[11px]">
+                          <MessageSquare size={10} className="mt-0.5" />
+                          <p>{item.content}</p>
+                        </div>
+                      )}
+                    </div>
+                  </td>
+
+                  <td className="bg-gray-50/50 rounded-r-2xl px-4 py-4 text-right">
+                    <button
+                      onClick={() => onDelete(item.id)}
+                      className="text-gray-300 hover:text-red-400 transition-colors"
+                    >
+                      <Trash2 size={18} />
+                    </button>
+                  </td>
+                </tr>
+              ))
+            )}
           </tbody>
         </table>
       </div>
